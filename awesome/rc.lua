89e6358059a868b53793c36765f86580c68a1467
kumarmo2 <kr.mohit6794@gmail.com>
2021-12-05 02:46:58 +0530

    updated awesome's window border.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -474,7 +474,8 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      -- properties = { border_width = beautiful.border_width,
+      properties = { border_width = 4,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,

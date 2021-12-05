d221648ecd6a433d3ec6537387e461e212a293ca
kumarmo2 <kr.mohit6794@gmail.com>
2021-12-05 18:07:35 +0530

    added nvim lua scripts

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,33 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+   use 'wbthomason/packer.nvim'
+   use {'junegunn/fzf', run = ' -> fzf#install()'}
+   use 'junegunn/fzf.vim'
+   use {'neoclide/coc.nvim', branch = 'release'}
+
+   use 'morhetz/gruvbox'
+   use 'machakann/vim-highlightedyank'
+   use 'kevinhwang91/rnvimr'
+
+   -- status line plugin
+   use 'itchyny/lightline.vim'
+
+   -- use 'neovim/nvim-lspconfig'
+--use 'hrsh7th/nvim-compe'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+

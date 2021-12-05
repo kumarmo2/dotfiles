9637327aaabab93d472465729f77b90b5d1635b9
kumarmo2 <kr.mohit6794@gmail.com>
2021-12-05 18:07:35 +0530

    added nvim lua scripts

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+
+local cmd = vim.cmd
+
+-- cmd('source '..lua_files_path..'/plugins.lua')
+cmd('source ~/.config/nvim/lua/settings.lua')
+cmd('source ~/.config/nvim/lua/plugins.lua')
+cmd('source ~/.config/nvim/lua/keymappings.lua')
+cmd('source ~/.config/nvim/vimfiles/coc.vim')
+
+
+
+
+
+
+

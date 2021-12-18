b792612744e7d6e8c4cb68fd98af0a69dd8b9cc5
kumarmo2 <kr.mohit6794@gmail.com>
2021-12-18 12:21:48 +0530

    updated some nvim config for csharp

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,9 @@ cmd('source ~/.config/nvim/lua/plugins.lua')
 cmd('source ~/.config/nvim/lua/keymappings.lua')
 cmd('source ~/.config/nvim/vimfiles/coc.vim')
 cmd('source ~/.config/nvim/vimfiles/lightline.vim')
+cmd('source ~/.config/nvim/lua/coc/init.lua')
+
+cmd('autocmd FileType javascript nnoremap <buffer> <C-_> I//<ESC>')
 
 
 

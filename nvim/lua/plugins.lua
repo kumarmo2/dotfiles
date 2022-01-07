cc78df28bd116d10038313c1e0898703cc98e0b0
kumarmo2 <kr.mohit6794@gmail.com>
2022-01-08 02:48:58 +0530

    added toggle term vim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -59,6 +59,8 @@ return require('packer').startup(function(use)
        },
    }
 
+   use 'akinsho/toggleterm.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

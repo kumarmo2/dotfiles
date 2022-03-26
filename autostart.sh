368d94c0be3ecdaecb67a041bc24182915f8bc76
kumarmo2 <kr.mohit6794@gmail.com>
2022-03-26 13:57:23 +0530

    more qtile config changes

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 # this script should be run on start.
 
 # set wallpaper
-hsetroot -extend  ~/configs/wallpapers/arcane/1188302.jpg
+hsetroot -extend  ~/configs/wallpapers/beautiful/wp2118464-219-wallpapers.jpg
 
 # swap escape and capslock
 setxkbmap -option caps:swapescape

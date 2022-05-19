23187b1913f6ad62910f1f8f2bba75664f40b807
kumarmo2 <kr.mohit6794@gmail.com>
2022-05-20 01:54:26 +0530

    working setup done for native lsp

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,45 +1,45 @@
- local vim = vim
- local setOption = vim.api.nvim_set_option
- local cmd = vim.cmd
- -- setOption('rnu', true)
- --setOption('nu', true)
- vim.o.rnu = true
- vim.o.nu = true
- -- highlight search terms
- vim.o.hlsearch = true
- -- ignore cases while searching
- vim.o.ignorecase = true
- vim.o.incsearch = true
- -- show cursor line
- -- setOption('cursorline', true)
- vim.o.cursorline = true
- -- removes the -- INSERT --. Since i am using lightline plugin which anyways
- -- shows the mode in it.
- vim.o.showmode = false
- vim.o.visualbell = true
- vim.o.wrap = false
- -- indentation behavior
- setOption('tabstop', 4)
- vim.o.tabstop = 4
- setOption('shiftwidth', 4)
- vim.o.shiftwidth = 4
- vim.o.expandtab = true
- vim.o.smartindent = true
- -- using big value for `scrolloff`, you can keep the cursor at the
- -- center
- -- vim.o.scrolloff = 999
- vim.o.colorcolumn = '120'
- -- shorter way to global options
- vim.o.swapfile = false
- vim.o.backup = false
- -- enable hidden buffers. By default, if there are unsaved changes in your
- -- current buffer, and you try moving to some another buffer, vim won't let you
- -- do that. By setting `hidden` it enables this behavior.
- vim.o.hidden = true
- setOption('syntax', 'enable')
- setOption('laststatus', 2)
- cmd('colorscheme nordfox')
- vim.o.background = 'dark'
- vim.termguicolors = true
- -- keep block cursor in normal, visual, command and insert modes.
- cmd('set guicursor=n-v-c-i:block-Cursor')
+local vim = vim
+local setOption = vim.api.nvim_set_option
+local cmd = vim.cmd
+-- setOption('rnu', true)
+--setOption('nu', true)
+vim.o.rnu = true
+vim.o.nu = true
+-- highlight search terms
+vim.o.hlsearch = true
+-- ignore cases while searching
+vim.o.ignorecase = true
+vim.o.incsearch = true
+-- show cursor line
+-- setOption('cursorline', true)
+vim.o.cursorline = true
+-- removes the -- INSERT --. Since i am using lightline plugin which anyways
+-- shows the mode in it.
+vim.o.showmode = false
+vim.o.visualbell = true
+vim.o.wrap = false
+-- indentation behavior
+setOption('tabstop', 4)
+vim.o.tabstop = 4
+setOption('shiftwidth', 4)
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+-- using big value for `scrolloff`, you can keep the cursor at the
+-- center
+-- vim.o.scrolloff = 999
+vim.o.colorcolumn = '120'
+-- shorter way to global options
+vim.o.swapfile = false
+vim.o.backup = false
+-- enable hidden buffers. By default, if there are unsaved changes in your
+-- current buffer, and you try moving to some another buffer, vim won't let you
+-- do that. By setting `hidden` it enables this behavior.
+vim.o.hidden = true
+setOption('syntax', 'enable')
+setOption('laststatus', 2)
+cmd('colorscheme gruvbox')
+vim.o.background = 'dark'
+vim.termguicolors = true
+-- keep block cursor in normal, visual, command and insert modes.
+cmd('set guicursor=n-v-c-i:block-Cursor')
--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -60,17 +60,23 @@ local function lsp_highlight_document(client)
   end
 end
 
-local function lsp_keymaps(bufnr)
+local function lsp_keymaps(bufnr, client)
   local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  if client.name == "omnisharp" then
+      -- For omnisharp, use Omnisharp plugin for just OmniSharpGotoDefinition
+      -- as GoToDefinition for dlls doesn't work correctly with native lsp.
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", ":OmniSharpGotoDefinition<CR>", opts)
+  else
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  end
   vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(
     bufnr,
@@ -88,7 +94,7 @@ M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
     client.resolved_capabilities.document_formatting = false
   end
-  lsp_keymaps(bufnr)
+  lsp_keymaps(bufnr, client)
   lsp_highlight_document(client)
 end
 

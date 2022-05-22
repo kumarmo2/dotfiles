c80c3c5d3e965e73dbe315ffe4401c0f160cb3aa
kumarmo2 <kr.mohit6794@gmail.com>
2022-05-22 09:09:01 +0530

    tmux and nvim init lua update

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,20 +4,17 @@ require('plugins')
 require('settings')
 
 -- TODO: migrate all vim files to lua
-cmd('source ~/.config/nvim/vimfiles/coc.vim')
-cmd('source ~/.config/nvim/vimfiles/coc-prettier.vim')
 
-require('coc/init');
-require('keymappings')
+require('user/lsp')
+require('user/nvim-cmp')
 require('treesitter')
 require('nerd-commenter')
 require('gitsigns-setup')
 require('fzf')
 require('nvim-tree-setup')
 require('toggleterm-setup')
-require('onedark-setup')
 require('lualine').setup()
+require('keymappings')
 
-
- cmd('source ~/.config/nvim/vimfiles/omnisharp.vim')
-
+cmd('source ~/.config/nvim/vimfiles/omnisharp.vim')
+-- cmd('autocmd FileType cs nnoremap gd :OmniSharpGotoDefinition<CR>')

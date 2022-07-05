b90070290979e3ce6a58f2bb67a6ab42654edb18
Mohit Kumar <mohit.kumar@sevenlakes.com>
2022-07-05 10:31:24 +0530

    updated neoformat config for js files to use prettier

--- nvim/lua/user/neoformat.lua
+++ nvim/lua/user/neoformat.lua
@@ -6,7 +6,8 @@ vim.api.nvim_exec(
 [[
     augroup fmt
         autocmd!
-        autocmd BufWritePre *.js undojoin | Neoformat
+        autocmd BufWritePre *.js undojoin | Neoformat prettier
+        autocmd BufWritePre *.go undojoin | Neoformat
     augroup END
 
 ]], true);

3fa6343a8dba34ac15b4471a2e808f99fadb3269
kumarmo2 <kr.mohit6794@gmail.com>
2022-08-06 17:49:52 +0530

    Some issue with filetype setting on open of file on startup custom script

--- nvim/lua/user/custom/au-commands.lua
+++ nvim/lua/user/custom/au-commands.lua
@@ -9,18 +9,24 @@ end
 local open_file_on_startup_if_not_provided = function()
         local bufnr = v.api.nvim_get_current_buf()
         local bufname = v.api.nvim_buf_get_name(bufnr);
+        local is_directory = v.fn.isdirectory(bufname);
         print("bufnr: ".. bufnr)
         print("bufname: ".. bufname)
-        if bufname == nil or bufname == '' then
+        print("isdirectory: ".. is_directory)
+
+        if bufname == nil or bufname == '' or is_directory == 1 then -- TODO: also need to do the same if its a directory
             print("no buf name")
+            -- TODO: the iss that is happening right now is that, when 
+            -- file is openend using this method,  either the filetype or lsp or both
+            -- are not being able to get set because of which no colorscheme, lsp is being enabled.
             utils.open_file_in_cur_dir()
         end
 end
 
 
 local vim_enter_setup = function ()
-    make_transparent()
-    open_file_on_startup_if_not_provided();
+   make_transparent()
+    -- open_file_on_startup_if_not_provided();
 end
 
 

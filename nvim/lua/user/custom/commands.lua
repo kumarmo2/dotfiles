63479151041d97ae7b3b0f4bdd180f50b48a6fea
kumarmo2 <kr.mohit6794@gmail.com>
2022-09-29 00:46:55 +0530

    handled transforming when only one line is visually selected. TODO: handle for multiple lines

--- nvim/lua/user/custom/commands.lua
+++ nvim/lua/user/custom/commands.lua
@@ -53,42 +53,62 @@ local getTableLength = function (t)
   return length
 end
 
-PrintSelectedContent = function ()
+local transform = function (lower)
   -- [bufnum, lnum, col, off]
   local start_pos = vim.fn.getpos("'<")
   local end_pos = vim.fn.getpos("'>")
   local startLine = start_pos[2]
   local endLine = end_pos[2]
 
-  print( "start_pos: " .. vim.inspect(start_pos));
-  print( "startLine: " .. startLine);
-  print( "end_pos: " .. vim.inspect(end_pos));
-  print( "endLine: " .. endLine);
-
+  -- lua vim.api.nvim_buf_set_text(0, 0, 0,0, 10, {'kumarmo2'}); 
   -- need to subtract 1 from startline as indices are 0 based.
-  local lines = api.nvim_buf_get_lines(0, startLine - 1, endLine, false);
-  print("total number of lines selected: "..getTableLength(lines))
+  local Lines = api.nvim_buf_get_lines(0, startLine - 1, endLine, false);
+  local numberOfLines = getTableLength(Lines)
+  -- PRINT("TOTAL NUMBER OF LINES SELECTED: "..NUMBEROFLINES)
 
-  for k, v in pairs(lines) do
-    print("key: "..k.." value: "..v)
-    if k == 1 then
-      
+  -- [[
+  -- cases:
+  --  1. when startRow == endRow i.e numberOfLines selected = 1
+  --  2. when startRow != endRow i.e numberOfLines selected > 1
+  -- ]]
+
+  local startRow = startLine - 1
+  local startCol = start_pos[3] -1
+  local endCol = end_pos[3]
+
+  -- TODO: handling for multiple lines
+  if numberOfLines == 1 then
+
+    local text = vim.api.nvim_buf_get_text(0, startRow,startCol, startRow, endCol, {} )[1]
+    local transformed;
+
+    if lower then
+      transformed = string.lower(text)
+    else
+      transformed = string.upper(text)
     end
+
+    vim.api.nvim_buf_set_text(0, startRow,startCol, startRow, endCol, {transformed} )
   end
-
-  print(vim.inspect(lines))
-
-
-
-
 end
 
 
+ToUpper = function ()
+  transform(false)
+end
+
+ToLower = function ()
+  transform(true)
+end
 
 api.nvim_create_user_command("Opaque", 'lua set_opaque()', {})
 api.nvim_create_user_command("Transparent", 'lua set_transparent()', {})
 
 
+local setKeyMap = vim.api.nvim_set_keymap
+
+setKeyMap('v', '<C-l>', ':lua ToLower()<CR>', {})
+setKeyMap('v', '<C-u>', ':lua ToUpper()<CR>', {})
 
 
 

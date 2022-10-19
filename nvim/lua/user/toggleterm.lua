948558ea3b627ed1f51b66c54190b0be52d98ee7
kumarmo2 <kr.mohit6794@gmail.com>
2022-10-19 09:42:12 +0530

    minor toggleterm(nvim) plugin config changes

--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -22,19 +22,20 @@ require("toggleterm").setup{
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
-  -- float_opts = {
+  float_opts = {
     -- The border key is *almost* the same as 'nvim_open_win'
     -- see :h nvim_open_win for details on borders however
     -- the 'curved' border is a custom border type
     -- not natively supported but implemented in this plugin.
     -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-    -- height = <value>,
+    height = 50,
+    width = 200,
     -- winblend = 3,
     -- highlights = {
       -- border = "Normal",
       -- background = "Normal",
     -- }
-  -- }
+  }
 }
 
 

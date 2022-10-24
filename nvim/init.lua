a30bca14553e253b49e62608849c905df4c45a62
kumarmo2 <kr.mohit6794@gmail.com>
2022-10-25 04:59:30 +0530

    more null-ls changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,25 @@
 local cmd = vim.cmd
-local v = vim;
-local bo = vim.bo; -- buffer level options.
+local v = vim
 local M = {}
-require('plugins')
-require('user.night_fox')
-require('settings')
--- TODO: migrate all vim files to lua
-require('user.lsp')
-require('user.nvim-cmp')
-require('user.treesitter')
-require('user.nerd-commenter')
-require('gitsigns-setup')
-require('user.nvim-tree')
-require('user.toggleterm')
-require('lualine').setup()
-require('user.neoformat')
-require('keymappings')
-require('user.telescope')
-require "user.custom.neovide"
-require('user.custom.commands')
-require "user.custom.au-commands"
-require "user.leap"
+require("plugins")
+require("user.night_fox")
+require("settings")
 
+require("user.lsp")
+require("user.lsp.null-ls")
+require("user.nvim-cmp")
+require("user.treesitter")
+require("user.nerd-commenter")
+require("gitsigns-setup")
+require("user.nvim-tree")
+require("user.toggleterm")
+require("lualine").setup()
+-- require('user.neoformat')
+require("keymappings")
+require("user.telescope")
+require("user.custom.neovide")
+require("user.custom.commands")
+require("user.custom.case-transformation")
 
 -- PrintX = function ()
     -- print("hello")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,112 +1,86 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+	packer_bootstrap =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
-return require('packer').startup(function(use)
-    -- My plugins here
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- My plugins here
+	use("wbthomason/packer.nvim")
 
-    -- use '/home/manya/code/nvim-plugins/practice-run.nvim'
-    -- nvim tree
-    use 'kyazdani42/nvim-web-devicons'
-    use 'kyazdani42/nvim-tree.lua'
-    use({
-        "folke/noice.nvim",
-        event = "VimEnter",
-        config = function()
-          require("noice").setup()
-        end,
-        requires = {
-          -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-          "MunifTanjim/nui.nvim",
-          "rcarriga/nvim-notify",
-          }
-      })
+	-- nvim tree
+	use("kyazdani42/nvim-web-devicons")
+	use("kyazdani42/nvim-tree.lua")
 
-    -- telescope 
-    use 'nvim-lua/plenary.nvim'
-    use {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-   use {'nvim-telescope/telescope-ui-select.nvim' }
-   use 'nvim-treesitter/playground'
+	-- telescope
+	use("nvim-lua/plenary.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-ui-select.nvim" })
 
-    -- nvim cmp
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-nvim-lsp'
+	-- nvim cmp
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lsp")
 
-    -- snippets
-    use 'L3MON4D3/LuaSnip'
+	-- snippets
+	use("L3MON4D3/LuaSnip")
 
-    -- vim movements
-    use 'ggandor/leap.nvim'
+	-- lsp
+	use("neovim/nvim-lspconfig") -- enable lsp
+	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
+	use("jose-elias-alvarez/null-ls.nvim") -- for better formatting
 
-    -- lsp
-    use 'neovim/nvim-lspconfig' -- enable lsp
-    use 'williamboman/nvim-lsp-installer' -- simple to use language server installer
-    use 'simrat39/rust-tools.nvim'
+	-- formatting
+	-- use 'sbdchd/neoformat'
 
-    -- dap
-    -- use 'mfussenegger/nvim-dap'
-    -- use 'leoluz/nvim-dap-go'
-    -- use 'rcarriga/nvim-dap-ui'
-    -- use 'theHamsta/nvim-dap-virtual-text'
-    -- use 'nvim-telescope/telescope-dap.nvim'
+	use("morhetz/gruvbox")
+	use("machakann/vim-highlightedyank")
 
-    -- formatting
-    use 'sbdchd/neoformat'
-    --
-    -- code foldings
-    use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-    use 'morhetz/gruvbox'
-    use 'machakann/vim-highlightedyank'
+	use("Hoffs/omnisharp-extended-lsp.nvim")
+	use("itchyny/vim-gitbranch")
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
+	-- for better syntax highlighting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			require("nvim-treesitter.install").update({ with_sync = true })
+		end,
+	})
+	use("nvim-treesitter/playground")
+	-- for commentings
+	use("preservim/nerdcommenter")
 
-    use 'Omnisharp/Omnisharp-vim'
+	-- nightfox and alike colorschemes
+	use("EdenEast/nightfox.nvim")
+	use("navarasu/onedark.nvim")
+	-- git integrations.
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
-    use 'itchyny/vim-gitbranch'
+	use("akinsho/toggleterm.nvim")
 
-    -- for better syntax highlighting
-        use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    }
+	use("ellisonleao/glow.nvim")
 
-    -- for commentings
-    use 'preservim/nerdcommenter'
+	use("famiu/bufdelete.nvim")
 
-    -- nightfox and alike colorschemes
-    use 'EdenEast/nightfox.nvim'
-    use 'navarasu/onedark.nvim'
-    -- git integrations.
-    use {
-        'lewis6991/gitsigns.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim'
-        },
-    }
-
-    use 'akinsho/toggleterm.nvim'
-
-    use 'ellisonleao/glow.nvim'
-
-    use 'famiu/bufdelete.nvim'
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require('packer').sync()
-    end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)

41a80116dea4c3f8855cb85167767211265d8e1d
kumarmo2 <kr.mohit6794@gmail.com>
2022-11-20 03:16:47 +0530

    removed coc and fzf last remaining references

--- nvim/lua/coc/init.lua
+++
@@ -1,6 +0,0 @@
-local cmd = vim.cmd
-
--- cmd('source ~/.config/nvim/coc/setup.lua');
-vim.g.coc_global_extensions = {'coc-omnisharp', 'coc-json', 'coc-css', 'coc-rust-analyzer', 'coc-tsserver',
-'coc-vimlsp', 'coc-sumneko-lua'}
-

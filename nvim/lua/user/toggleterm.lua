48197a2cb4a1089ec7acdc0af2bf25cebd7477b4
kumarmo2 <kr.mohit6794@gmail.com>
2022-12-11 03:50:04 +0530

    more changes to common configs

--- nvim/lua/user/toggleterm.lua
+++ nvim/lua/user/toggleterm.lua
@@ -1,14 +1,14 @@
-require("toggleterm").setup{
+require("toggleterm").setup {
   -- size can be a number or function which is passed the current terminal
   -- size = 20 | function(term)
-    -- if term.direction == "horizontal" then
-      -- return 15
-    -- elseif term.direction == "vertical" then
-      -- return vim.o.columns * 0.4
-    -- end
+  -- if term.direction == "horizontal" then
+  -- return 15
+  -- elseif term.direction == "vertical" then
+  -- return vim.o.columns * 0.4
+  -- end
   -- end,
   size = 20,
-  open_mapping = [[<c-\>]],
+  open_mapping = [[<C-\>]],
   -- on_open = fun(t: Terminal), -- function to run when the terminal opens
   -- on_close = fun(t: Terminal), -- function to run when the terminal closes
   hide_numbers = true, -- hide the number column in toggleterm buffers
@@ -23,18 +23,16 @@ require("toggleterm").setup{
   shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
   -- float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-    -- height = <value>,
-    -- winblend = 3,
-    -- highlights = {
-      -- border = "Normal",
-      -- background = "Normal",
-    -- }
+  -- The border key is *almost* the same as 'nvim_open_win'
+  -- see :h nvim_open_win for details on borders however
+  -- the 'curved' border is a custom border type
+  -- not natively supported but implemented in this plugin.
+  -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+  -- height = <value>,
+  -- winblend = 3,
+  -- highlights = {
+  -- border = "Normal",
+  -- background = "Normal",
+  -- }
   -- }
 }
-
-

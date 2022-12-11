1a175a7d3c9877ab2cbdb40deba8d6ad0a31692d
kumarmo2 <kr.mohit6794@gmail.com>
2022-12-11 15:48:28 +0530

    minor change in one-dark-pro

--- nvim/lua/user/colorschemes/onedarkpro.lua
+++ nvim/lua/user/colorschemes/onedarkpro.lua
@@ -31,7 +31,7 @@ require("onedarkpro").setup({
 		undercurl = true, -- Use undercurl styles?
 
 		cursorline = true, -- Use cursorline highlighting?
-		transparency = true, -- Use a transparent background?
+		transparency = false, -- Use a transparent background?
 		terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
 		highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
 	},

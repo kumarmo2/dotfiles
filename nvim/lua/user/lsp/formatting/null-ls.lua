f694e5df941b771f396ab2a6dac43ea5b61ab450
kumarmo2 <kr.mohit6794@gmail.com>
2022-12-11 05:42:50 +0530

    refactored lsp formatting, using null-ls wherever possible for formatting

--- nvim/lua/user/lsp/formatting/null-ls.lua
+++ nvim/lua/user/lsp/formatting/null-ls.lua
@@ -4,7 +4,14 @@ local vim = vim
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 -- local completion = null_ls.builtins.completion
-
+-- [[
+-- NOTE:
+-- in this config file we are only setting up the null-ls. i.e we are telling what
+-- formatters, linters etc to use. This setup will make sure when a call is made to `vim.lsp.buf.format` and null-ls
+-- ends up formatting the document, it will format according to this setup.
+-- check the lsp on_attach handler where I am setting up the lsp based formatting.
+--
+-- ]]
 -- NOTE: Need to make sure the sources are available in $PATH.
 local config = {
   sources = {
@@ -12,17 +19,12 @@ local config = {
     formatting.stylua,
     formatting.prettier,
     formatting.csharpier,
+    formatting.rustfmt,
 
     -- linters/diagnostics
     diagnostics.eslint,
   },
+  -- debug = true
 }
 
 null_ls.setup(config)
-
--- Setting auto-command for format on save for particular languages.
-local group = vim.api.nvim_create_augroup("nvim_auto_format", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.js", "*.lua", "*.cs", "*.rs" },
-  command = "lua vim.lsp.buf.format()",
-})

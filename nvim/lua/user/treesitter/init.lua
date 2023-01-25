dc04a6607e77a23e23a5db66c98dd3707e5fe566
kumarmo2 <kr.mohit6794@gmail.com>
2023-01-26 00:40:54 +0530

    added treesitter based code folding

--- nvim/lua/user/treesitter/init.lua
+++ nvim/lua/user/treesitter/init.lua
@@ -1,63 +1,56 @@
-local vim = vim;
-local opt = vim.opt;
+local vim = vim
+local opt = vim.opt
 
-require'nvim-treesitter.configs'.setup {
-  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"rust", "c", "go",  "css", "html", "lua", "javascript", "python", "c_sharp"} ,
+require("nvim-treesitter.configs").setup({
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "rust", "c", "go", "css", "html", "lua", "javascript", "python", "c_sharp" },
 
-  -- Install languages synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+	-- Install languages synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-  -- List of parsers to ignore installing
-  -- ignore_install = { "javascript" },
+	-- List of parsers to ignore installing
+	-- ignore_install = { "javascript" },
 
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
 
-    -- list of language that will be disabled
-    -- disable = { "c", "rust" },
+		-- list of language that will be disabled
+		-- disable = { "c", "rust" },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  }
-}
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = "o",
+			toggle_hl_groups = "i",
+			toggle_injected_languages = "t",
+			toggle_anonymous_nodes = "a",
+			toggle_language_display = "I",
+			focus_language = "f",
+			unfocus_language = "F",
+			update = "R",
+			goto_node = "<cr>",
+			show_help = "?",
+		},
+	},
+})
 
 -- code folding starts
 
--- opt.foldmethod = "expr"
--- opt.foldexpr = "nvim_treesitter#foldexpr()"
-
--- local callback = function ()
-    -- print("inside callback")
-    -- vim.api.nvim_command('normal zR')
--- end
-
--- local group = vim.api.nvim_create_augroup("OpenCodeFoldsByDefault", { clear = true});
--- vim.api.nvim_create_autocmd({"BufReadPost", "FileReadPost", "BufEnter"}, { pattern = "*", callback = callback, group = group})
+opt.foldlevel = 20
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- code folding ends

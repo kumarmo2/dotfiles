e738785f94b4c0a8a15ae97d1cdf133488afe1e8
kumarmo2 <kr.mohit6794@gmail.com>
2023-02-16 00:59:26 +0530

    updated formatter from null-ls to omnisharp

--- nvim/lua/user/lsp/formatting/init.lua
+++ nvim/lua/user/lsp/formatting/init.lua
@@ -1,15 +1,18 @@
-local vim = vim;
+local vim = vim
 local M = {}
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      -- apply whatever logic you want (in this example, we'll only use null-ls)
-      -- to add handling for other clients as well.
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			-- to add handling for other clients as well.
+			if client.name == "omnisharp" then
+				return true
+			end
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts#neovim-08
@@ -18,16 +21,16 @@ end
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 M.setup_lsp_formatting = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        lsp_formatting(bufnr)
-      end,
-    })
-  end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
 end
 
 return M

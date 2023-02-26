ef39f280d2307dba1b92dd329357f1a23ae13083
kumarmo2 <kr.mohit6794@gmail.com>
2023-02-27 02:34:21 +0530

    added some integration with mason

---
+++ nvim/lua/user/lsp/servers/rust/init.lua
@@ -0,0 +1,19 @@
+local rt = require("rust-tools")
+local common_on_attach = require("user.lsp.handlers").on_attach
+
+-- rust-tools plugin takes care of setting up the server capabilities.
+-- in opts.on_attach, we have some common functionalities like key-mappings
+-- , document formatting etc.
+-- NOTE: see below script to turn on/off inlay_hints.
+-- lua require('rust-tools').inlay_hints.set()
+-- lua require('rust-tools').inlay_hints.unset()
+
+local M = {}
+M.setup = function()
+	rt.setup({
+		server = {
+			on_attach = common_on_attach,
+		},
+	})
+end
+return M

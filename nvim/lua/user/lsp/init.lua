4c3230e86a9d00b929d42abf40f5dcb5b83ea8ed
kumarmo2 <kr.mohit6794@gmail.com>
2023-02-27 10:58:42 +0530

    minor changes for mason and refactoring

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -9,10 +9,18 @@ mason.setup({})
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "rust_analyzer", "omnisharp" },
 })
+
+local lspconfig = require("lspconfig")
+
 local common_on_attach = require("user.lsp.handlers").on_attach
 local client_capablities = require("user.lsp.handlers").capabilities
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-require("user.lsp.servers.lua").setup()
-require("user.lsp.servers.rust").setup()
-require("lspconfig").tsserver.setup({ on_attach = common_on_attach, capabilities = client_capablities })
+local lsp_options = { on_attach = common_on_attach, capabilities = client_capablities }
+-- NOTE: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
+-- Language servers setup.
+
+lspconfig.lua_ls.setup(lsp_options) -- lua
+lspconfig.tsserver.setup(lsp_options) -- js/ts
+require("user.lsp.servers.rust").setup() -- rust
+require("user.lsp.servers.omnisharp").setup() -- csharp

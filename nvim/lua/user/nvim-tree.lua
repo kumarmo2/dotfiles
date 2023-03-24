77dd21bd38d43df57fc5b7155262c812fc28bb4e
kumarmo2 <kr.mohit6794@gmail.com>
2023-03-24 08:28:57 +0530

    nvim-tree config and stylua config update

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -2,42 +2,36 @@ local setKeyMap = vim.api.nvim_set_keymap
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
-local status_ok, nvim_tree = pcall(require, "nvim-tree")
+local status_ok, nvim_tree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
 if not config_status_ok then
   return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup {
+nvim_tree.setup({
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
   respect_buf_cwd = true,
   hijack_directories = {
     enable = true,
-    auto_open = true
+    auto_open = true,
   },
   diagnostics = {
     enable = false,
     icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+      hint = '',
+      info = '',
+      warning = '',
+      error = '',
     },
   },
   update_focused_file = {
@@ -51,12 +45,12 @@ nvim_tree.setup {
     timeout = 500,
   },
   renderer = {
-    root_folder_modifier = ":t",
+    root_folder_modifier = ':t',
     icons = {
       webdev_colors = true,
-      git_placement = "before",
-      padding = " ",
-      symlink_arrow = " ➛ ",
+      git_placement = 'before',
+      padding = ' ',
+      symlink_arrow = ' ➛ ',
       show = {
         file = true,
         folder = true,
@@ -64,42 +58,41 @@ nvim_tree.setup {
         git = true,
       },
       glyphs = {
-        default = "",
-        symlink = "",
+        default = '',
+        symlink = '',
         folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
+          arrow_closed = '',
+          arrow_open = '',
+          default = '',
+          open = '',
+          empty = '',
+          empty_open = '',
+          symlink = '',
+          symlink_open = '',
         },
         git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
+          unstaged = '✗',
+          staged = '✓',
+          unmerged = '',
+          renamed = '➜',
+          untracked = '★',
+          deleted = '',
+          ignored = '◌',
         },
-      }
-    }
-
+      },
+    },
   },
   view = {
     width = 40,
     -- height = 30,
     hide_root_folder = false,
-    side = "right",
+    side = 'right',
     mappings = {
       custom_only = false,
       list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
+        { key = { 'l', '<CR>', 'o' }, cb = tree_cb('edit') },
+        { key = 'h', cb = tree_cb('close_node') },
+        { key = 'v', cb = tree_cb('vsplit') },
       },
     },
     number = false,
@@ -108,23 +101,21 @@ nvim_tree.setup {
   actions = {
     open_file = {
       quit_on_open = false,
-      resize_window = true
+      resize_window = true,
     },
     change_dir = {
       -- this is used so that nvim tree won't go above the directory in which it was opened.
       -- restrict_above_cwd = true
-    }
-  }
-}
-
-
+    },
+  },
+})
 
 -- if last window is nvim-tree. quit the nvim
 local buf_enter_callback = function(opts)
-  if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-    vim.cmd "quit"
+  if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
+    vim.cmd('quit')
   end
 end
 
-local group = vim.api.nvim_create_augroup("NvimTreeCloseIfLast", { clear = true });
-vim.api.nvim_create_autocmd("BufEnter", { callback = buf_enter_callback, group = group })
+local group = vim.api.nvim_create_augroup('NvimTreeCloseIfLast', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', { callback = buf_enter_callback, group = group })

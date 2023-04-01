0a5aa368290d83654c2be50e2f8205b29b10d854
kumarmo2 <kr.mohit6794@gmail.com>
2023-04-01 12:48:50 +0530

    basic setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,37 @@
 local cmd = vim.cmd
-local v = vim
+local vim = vim
 local M = {}
-require('impatient')
-require('plugins')
-require('user.greeters.alpha')
+-- install lazy plugin manager starts.
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+print(vim.inspect(vim.opt.rtp))
+
+-- install lazy plugin manager ends.
+
+local plugins = require('plugins.plugins')
+
+-- print(vim.inspect(plugins))
+require('lazy').setup(plugins, { defaults = { lazy = false } })
+
+-- require('impatient')
+-- require('plugins')
+-- require('user.greeters.alpha')
 -- require("user.colorschemes.night_fox")
 -- require("user.colorschemes.catppuccin")
--- require("user.colorschemes.tokyonight")
+require('user.colorschemes.tokyonight')
 -- require("user.colorschemes.gruvbox")
 -- v.cmd("colorscheme oxocarbon")
-require('user.colorschemes.onedarkpro')
+-- require('user.colorschemes.onedarkpro')
 require('settings')
 
 require('user.lsp')
@@ -26,6 +48,6 @@ require('keymappings')
 -- require('user.git_integrations.diffview')
 require('user.telescope')
 require('user.custom.case-transformation')
-require('user.session-persistence')
+-- require('user.session-persistence')
 
 return M

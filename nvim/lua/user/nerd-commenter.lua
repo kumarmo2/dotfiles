50bcf386c42b53a358edd8702d487f8cc57f2edd
kumarmo2 <kr.mohit6794@gmail.com>
2023-04-19 11:21:50 +0530

    updated nerd-commenter mapping for neovide

--- nvim/lua/user/nerd-commenter.lua
+++ nvim/lua/user/nerd-commenter.lua
@@ -4,5 +4,12 @@ local setKeyMap = v.api.nvim_set_keymap
 v.g.NERDSpaceDelims = true
 local mapping_options = { noremap = true, silent = true }
 
-setKeyMap("n", "<C-_>", "<plug>NERDCommenterToggle", mapping_options)
-setKeyMap("v", "<C-_>", "<plug>NERDCommenterToggle", mapping_options)
+setKeyMap('n', '<C-_>', '<plug>NERDCommenterToggle', mapping_options)
+setKeyMap('v', '<C-_>', '<plug>NERDCommenterToggle', mapping_options)
+
+local is_neovide = v.g.neovide or false
+if is_neovide then
+  -- for neovide, the below mapping works
+  setKeyMap('n', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)
+  setKeyMap('v', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)
+end

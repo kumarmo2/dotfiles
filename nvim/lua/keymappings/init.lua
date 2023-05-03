ef9f880773f08f029a7a35f4e5a20ba66adadd40
kumarmo2 <kr.mohit6794@gmail.com>
2023-05-03 19:42:47 +0530

    updated some mappings

--- nvim/lua/keymappings/init.lua
+++ nvim/lua/keymappings/init.lua
@@ -1,68 +1,68 @@
 -- set function ref to make it easier
 local vim = vim
 local setKeyMap = vim.api.nvim_set_keymap
-require("keymappings.toggle-cmdheight")
+require('keymappings.toggle-cmdheight')
 
 -- set leader key to ','
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local opts = { noremap = true, silent = true }
 
-setKeyMap("n", "\\", ":noh<CR>", opts)
+setKeyMap('n', '\\', ':noh<CR>', opts)
 
-setKeyMap("n", "<Leader>w", ":w<CR>", opts)
-setKeyMap("n", "<Leader><CR>", ":q<CR>", opts)
+setKeyMap('n', '<Leader>w', ':w<CR>', opts)
+setKeyMap('n', '<Leader><CR>', ':q<CR>', opts)
 
 -- movements
-setKeyMap("n", "<C-d>", "<C-d>zz", opts)
-setKeyMap("n", "<C-u>", "<C-u>zz", opts)
+setKeyMap('n', '<C-d>', '<C-d>zz', opts)
+setKeyMap('n', '<C-u>', '<C-u>zz', opts)
 
 -- prev buffer
-setKeyMap("n", "<leader><leader>", "<C-^>", { noremap = true })
+setKeyMap('n', '<leader><leader>', '<C-^>', { noremap = true })
 
 -- better window movement
-setKeyMap("n", "<Leader>h", "<C-w>h", opts)
-setKeyMap("n", "<Leader>j", "<C-w>j", opts)
-setKeyMap("n", "<Leader>k", "<C-w>k", opts)
-setKeyMap("n", "<Leader>l", "<C-w>l", opts)
+setKeyMap('n', '<Leader>h', '<C-w>h', opts)
+setKeyMap('n', '<Leader>j', '<C-w>j', opts)
+setKeyMap('n', '<Leader>k', '<C-w>k', opts)
+setKeyMap('n', '<Leader>l', '<C-w>l', opts)
 
 -- tabs management
-setKeyMap("n", "<leader>]", ":tabnext<CR>", opts)
-setKeyMap("n", "<leader>[", ":tabprevious<CR>", opts)
-setKeyMap("n", "<leader>tc", ":tabclose<CR>", opts)
+setKeyMap('n', '<leader>]', ':tabnext<CR>', opts)
+setKeyMap('n', '<leader>[', ':tabprevious<CR>', opts)
+setKeyMap('n', '<leader>tc', ':tabclose<CR>', opts)
 -- nnoremap <leader>l :tabnext<CR>
 
 -- splits management starts
 -- vertical splits resize
-setKeyMap("n", "_", ":vertical resize -2<CR>", opts)
-setKeyMap("n", "+", ":vertical resize +2<CR>", opts)
+setKeyMap('n', '_', ':vertical resize -2<CR>', opts)
+setKeyMap('n', '+', ':vertical resize +2<CR>', opts)
 
 -- horizontal splits resize
-setKeyMap("n", "-", ":resize -2<CR>", opts)
-setKeyMap("n", "=", ":resize +2<CR>", opts)
+setKeyMap('n', '-', ':resize -2<CR>', opts)
+setKeyMap('n', '=', ':resize +2<CR>', opts)
 
 -- splits management ends
 
 -- buffers
 -- delete current buffer.
-setKeyMap("n", "<leader>dd", ":Bdelete<CR>", opts)
+setKeyMap('n', '<leader>dd', ':Bdelete<CR>', opts)
 
 -- better visual indenting
-setKeyMap("v", "<", "<gv", opts)
-setKeyMap("v", ">", ">gv", opts)
+setKeyMap('v', '<', '<gv', opts)
+setKeyMap('v', '>', '>gv', opts)
 
 -- setKeyMap('n', 'vx', ':lua require("stackmap").select()<CR>', { noremap = true });
 
-setKeyMap("n", "<leader>ss", ":so %<CR>", { noremap = true })
-setKeyMap("n", "<leader>th", ":Telescope help_tags<CR>", { noremap = true })
-setKeyMap("n", "D", "0d$", { noremap = true, silent = true })
+setKeyMap('n', '<leader>ss', ':so %<CR>', { noremap = true })
+setKeyMap('n', '<leader>th', ':Telescope help_tags<CR>', { noremap = true })
+setKeyMap('n', 'D', '0d$', { noremap = true, silent = true })
 
 -- toggle cmd height
-setKeyMap("n", "<C-]>", ":lua toggle_cmd_height()<CR>", { noremap = true, silent = true })
+setKeyMap('n', '<C-]>', ':lua toggle_cmd_height()<CR>', { noremap = true, silent = true })
 
 -- open nvim config folder in new tab
-setKeyMap("n", "<leader>c", ":tabnew ~/configs/nvim/init.lua<CR>", { noremap = true, silent = true })
+setKeyMap('n', '<leader>c', ':tabnew ~/configs/nvim/init.lua<CR>', { noremap = true, silent = true })
 
 -- nvim tree
-setKeyMap("n", "<Leader>n", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-setKeyMap("n", "<Leader>r", ":NvimTreeRefresh<CR>", { noremap = true, silent = false })
+setKeyMap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+setKeyMap('n', '<Leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = false })
--- nvim/lua/user/nerd-commenter.lua
+++ nvim/lua/user/nerd-commenter.lua
@@ -6,10 +6,5 @@ local mapping_options = { noremap = true, silent = true }
 
 setKeyMap('n', '<C-_>', '<plug>NERDCommenterToggle', mapping_options)
 setKeyMap('v', '<C-_>', '<plug>NERDCommenterToggle', mapping_options)
-
-local is_neovide = v.g.neovide or false
-if is_neovide then
-  -- for neovide, the below mapping works
-  setKeyMap('n', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)
-  setKeyMap('v', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)
-end
+setKeyMap('n', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)
+setKeyMap('v', '<C-/>', '<plug>NERDCommenterToggle', mapping_options)

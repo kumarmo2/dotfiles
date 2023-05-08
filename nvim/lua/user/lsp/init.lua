365a17bf245457af86840aa49691034e783b8502
kumarmo2 <kr.mohit6794@gmail.com>
2023-05-08 11:44:00 +0530

    fixed some tsserver lsp issue with react and qwik projects

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,19 +1,19 @@
-local found, mason = pcall(require, "mason")
+local found, mason = pcall(require, 'mason')
 
 if found ~= true then
-	return
+  return
 end
 
 mason.setup({})
 
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "rust_analyzer", "omnisharp" },
+require('mason-lspconfig').setup({
+  ensure_installed = { 'lua_ls', 'rust_analyzer', 'omnisharp' },
 })
 
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 
-local common_on_attach = require("user.lsp.handlers").on_attach
-local client_capablities = require("user.lsp.handlers").capabilities
+local common_on_attach = require('user.lsp.handlers').on_attach
+local client_capablities = require('user.lsp.handlers').capabilities
 
 local lsp_options = { on_attach = common_on_attach, capabilities = client_capablities }
 -- NOTE: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
@@ -21,6 +21,10 @@ local lsp_options = { on_attach = common_on_attach, capabilities = client_capabl
 -- Language servers setup.
 
 lspconfig.lua_ls.setup(lsp_options) -- lua
-lspconfig.tsserver.setup(lsp_options) -- js/ts
-require("user.lsp.servers.rust").setup() -- rust
-require("user.lsp.servers.omnisharp").setup() -- csharp
+-- lspconfig.jdtls.setup(lsp_options)
+lspconfig.pylsp.setup({})
+
+lspconfig.yamlls.setup({})
+require('user.lsp.servers.rust').setup() -- rust
+require('user.lsp.servers.omnisharp').setup() -- csharp
+require('user.lsp.servers.tsserver').setup() -- js/ts

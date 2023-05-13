bf3657bf7fe79a2de4988539cd2e35d907d4bcfc
kumarmo2 <kr.mohit6794@gmail.com>
2023-05-14 02:00:15 +0530

    tmux plugin and alacritty minor changes

--- nvim/lua/user/colorschemes/catppuccin.lua
+++ nvim/lua/user/colorschemes/catppuccin.lua
@@ -1,15 +1,15 @@
-require("catppuccin").setup({
-  flavour = "macchiato", -- latte, frappe, macchiato, mocha
+require('catppuccin').setup({
+  flavour = 'macchiato', -- latte, frappe, macchiato, mocha
   background = { -- :h background
-    light = "latte",
-    dark = "mocha",
+    light = 'latte',
+    dark = 'mocha',
   },
-  compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+  compile_path = vim.fn.stdpath('cache') .. '/catppuccin',
   transparent_background = false,
   -- term_colors = false,
   -- dim_inactive = {
   -- enabled = false,
-  shade = "dark",
+  shade = 'dark',
   -- percentage = 0.15,
   -- },
   -- styles = {
@@ -37,3 +37,5 @@ require("catppuccin").setup({
   -- -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   -- },
 })
+
+vim.cmd('colorscheme catppuccin')

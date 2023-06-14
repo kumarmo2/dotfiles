6fa6e1d8e9056093e8e95ae7217da5fc80c3e53a
kumarmo2 <kr.mohit6794@gmail.com>
2023-06-14 18:20:08 +0530

    added trouble toggle plugin

---
+++ nvim/lua/plugins/trouble_helper.lua
@@ -0,0 +1,14 @@
+return {
+  {
+
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', name = 'nvim_tree_web_devicons' },
+    config = function()
+      require('trouble').setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
+}

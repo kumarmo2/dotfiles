f9a4fb564eab96a44b4c0e3c74a3da1deb457416
Mohit Kumar <kumarmo2@debian.kumarmo2>
2023-08-11 09:59:07 +0530

    switch tmux session instead of creation if session already exists

--- scripts/bash/tmux_fzf.sh
+++ scripts/bash/tmux_fzf.sh
@@ -20,7 +20,19 @@ if [ -n "$selected_dir" ]; then
   # Get the base name of the selected directory
   dir_name=$(basename "$selected_dir")
 
-  
+  # if tere is a tmux session with same name, switch to it instead of creating a new one.
+   if $INSIDE_TMUX; then
+     tmux has-session -t "$dir_name" 2>/dev/null
+     session_exists=$?
+   else
+     session_exists=1
+   fi
+
+  # Create or switch to the session based on the existence check
+  if [ "$session_exists" -eq 0 ]; then
+    tmux switch-client -t "$dir_name"
+  else
+
   # If already inside tmux, unset $TMUX and create a new session in the selected directory
   if [ -n "$TMUX" ]; then
     unset TMUX
@@ -30,5 +42,7 @@ if [ -n "$selected_dir" ]; then
     # If not inside tmux, start a new tmux session and detach
     tmux new-session -c "$selected_dir" -s "$dir_name"
   fi
+  fi
 fi
+
 

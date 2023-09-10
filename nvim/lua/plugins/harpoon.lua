72d608ceac5610ff547fd09ce8ad081a3e5eab2a
kumarmo2 <kr.mohit6794@gmail.com>
2023-09-10 23:26:55 +0530

    added harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,13 @@
+local vim = vim
+local binding_opts = {}
+return {
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>,', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', binding_opts)
+      vim.api.nvim_set_keymap('n', '<leader>m', ':lua require("harpoon.mark").add_file()<CR>', binding_opts)
+      vim.api.nvim_set_keymap('n', '<leader>]', ':lua require("harpoon.ui").nav_next()<CR>', binding_opts)
+      vim.api.nvim_set_keymap('n', '<leader>[', ':lua require("harpoon.ui").nav_prev()<CR>', binding_opts)
+    end,
+  },
+}

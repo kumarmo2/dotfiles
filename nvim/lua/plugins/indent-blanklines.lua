02f8012413ed698acb1437d9669490eac1a2648f
kumarmo2 <kr.mohit6794@gmail.com>
2023-10-21 18:20:30 +0530

    remove highlighting first line of the scope

--- nvim/lua/plugins/indent-blanklines.lua
+++ nvim/lua/plugins/indent-blanklines.lua
@@ -8,6 +8,12 @@ return {
     main = 'ibl',
     config = function()
       require('ibl').setup({
+        enabled = true,
+        scope = {
+          enabled = true,
+          show_start = false,
+          highlight = { 'Function', 'Label' },
+        },
         -- for example, context is off by default, use this to turn it on
         -- show_current_context = true,
         -- show_trailing_blankline_indent = true,

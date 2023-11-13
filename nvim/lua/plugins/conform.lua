b9809f3f7f479ca3eb5eddea273f3914feb45766
kumarmo2 <kr.mohit6794@gmail.com>
2023-11-14 02:28:23 +0530

    added formatter for sql.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -9,6 +9,7 @@ return {
       conform_config.formatters_by_ft.lua = { 'stylua' }
       conform_config.formatters_by_ft.rust = { 'rustfmt' }
       conform_config.formatters_by_ft.go = { 'gofmt' }
+      conform_config.formatters_by_ft.sql = { 'sql_formatter' }
       conform_config.format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,

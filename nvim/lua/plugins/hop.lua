100d24ccb8281e2f57ee4eb4ddb047410e6bf12d
kumarmo2 <kr.mohit6794@gmail.com>
2024-03-09 00:06:57 +0530

    added back the hop.nvim and changed its bindings.

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -4,12 +4,12 @@ return {
     'smoka7/hop.nvim',
     version = '*',
     opts = {},
-    keys = { '<leader>/'},
+    keys = { '<leader>hf' }, -- <leader>hf: hop find
     config = function()
       local hop = require('hop')
       local directions = require('hop.hint').HintDirection
       hop.setup()
-      vim.keymap.set('', '<leader>/', function()
+      vim.keymap.set('', '<leader>hf', function() -- <leader>fh: hop find
         hop.hint_char1({ direction = directions.ACross, current_line_only = false })
       end, { remap = true })
     end,

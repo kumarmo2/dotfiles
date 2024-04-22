fc7b0c5d0995a2960eeb2603331826d11be4ade5
kumarmo2 <kr.mohit6794@gmail.com>
2024-04-22 13:33:56 +0530

    use native fzf for fuzzy finding in telescope.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,7 +11,24 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     keys = keys,
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+
+        -- If encountering errors, see telescope-fzf-native README for install instructions
+        'nvim-telescope/telescope-fzf-native.nvim',
+
+        -- `build` is used to run some command when the plugin is installed/updated.
+        -- This is only run then, not every time Neovim starts up.
+        build = 'make',
+
+        -- `cond` is a condition used to determine whether this plugin should be
+        -- installed and loaded.
+        cond = function()
+          return vim.fn.executable('make') == 1
+        end,
+      },
+    },
     config = function()
       local setKeyMap = vim.keymap.set
       local opts = { noremap = true, silent = true }

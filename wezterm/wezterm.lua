65f56a4494b6f264de21d6a35a9955cc248e6695
kumarmo2 <kr.mohit6794@gmail.com>
2024-08-19 12:04:17 +0530

    updated fonts on wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local bindings = require("key_bindings")
+local wezterm = require 'wezterm'
 
 local config = {}
 bindings.add_key_bindings(config)
@@ -8,6 +9,8 @@ config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.hide_tab_bar_if_only_one_tab = true
 config.disable_default_key_bindings = true
 config.window_background_opacity = 1;
+config.font =
+    wezterm.font('JetBrains Mono')
 
 -- Spawn a bash shell in non-login shell
 

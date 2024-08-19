afc9dca32303c88344c22cae0e1c77975566331e
kumarmo2 <kr.mohit6794@gmail.com>
2024-08-19 12:08:29 +0530

    disabled codeium by default

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -28,7 +28,7 @@ return {
     event = 'BufEnter',
     config = function()
       -- Change '<C-g>' here to any keycode you like.
-      vim.g.codeium_enabled = true
+      vim.g.codeium_enabled = false
       vim.keymap.set('i', '<C-g>', function()
         return vim.fn['codeium#Accept']()
       end, { expr = true })

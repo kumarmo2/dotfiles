78c43ae630511bba4cb4ccee9f561fd202ff2920
kumarmo2 <kr.mohit6794@gmail.com>
2024-12-20 13:34:04 +0530

    updated blink, deprecated nvim-cmp and updated plugins

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -7,7 +7,11 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'enter',
+        ['<C-k>'] = { 'select_prev', },
+        ['<C-j>'] = { 'select_next', },
+      },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
--- nvim/lua/plugins/trouble_helper.lua
+++ nvim/lua/plugins/trouble_helper.lua
@@ -6,7 +6,6 @@ return {
   {
     'folke/trouble.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', name = 'nvim_tree_web_devicons' },
-    cmd = { 'TroubleToggle' },
     lazy = false,
     config = function()
       local opts = { noremap = true, silent = false }

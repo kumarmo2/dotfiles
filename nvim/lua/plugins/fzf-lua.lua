6e654f600230f418cfa92746af03276d909e72e2
kumarmo2 <kr.mohit6794@gmail.com>
2024-12-22 21:21:44 +0530

    added fzf-lua and deprecated telescope

---
+++ nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,25 @@
+-- local keys = vim.tbl_keys(keys_to_command_map)
+return {
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+      local keys_to_command_map = {
+        ['<leader>p'] = ':FzfLua files<CR>',
+        ['<leader>;'] = ':FzfLua buffers<CR>',
+        ['<leader>fg'] = ':FzfLua live_grep<CR>',
+        ['<leader>gf'] = ':Telescope git_files<CR>',
+        ['<leader>th'] = ':FzfLua helptags<CR>',
+      }
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_exec2("FzfLua register_ui_select", {})
+      for k, v in pairs(keys_to_command_map) do
+        vim.keymap.set('n', k, v, opts)
+      end
+    end
+  }
+}

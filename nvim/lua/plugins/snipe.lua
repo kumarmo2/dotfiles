1e2573bd602ca0eec702ed7f16d7ca4d2e8c7218
kumarmo2 <kr.mohit6794@gmail.com>
2025-01-11 05:07:43 +0530

    updated snipe config a bit.

--- nvim/lua/plugins/snipe.lua
+++ nvim/lua/plugins/snipe.lua
@@ -1,17 +1,16 @@
 return {
   {
     "leath-dub/snipe.nvim",
-    keys = { "<leader>sb" },
-    desc = "Open snipe buffer menu",
+    keys = {
+      { "fj", function() require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu" }
+    },
     config = function()
-      local snipe = require("snipe")
-      snipe.setup()
-
-      vim.keymap.set("n", "<leader>sb", function()
-        print("snipe buffers called")
-        snipe.create_buffer_menu_toggler({ max_path_width = 2 })()
-      end)
+      local snipe = require('snipe')
+      snipe.setup({
+        ui = {
+          position = "center"
+        }
+      })
     end
-
   }
 }

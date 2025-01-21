197056ff15b8cd8f51bd0929105b2463f912307d
kumarmo2 <kr.mohit6794@gmail.com>
2025-01-21 14:46:50 +0530

    remove decorations for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,12 +6,13 @@ bindings.add_key_bindings(config)
 
 -- config.color_scheme = 'Everforest Dark Hard (Gogh)'
 -- config.color_scheme = 'Everforest Light (Gogh)'
--- config.color_scheme = 'Everforest Dark (Gogh)'
-config.color_scheme = 'Everforest Dark Hard (Gogh)'
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+-- config.color_scheme = 'Everforest Dark Hard (Gogh)'
 
 config.hide_tab_bar_if_only_one_tab = true
 config.disable_default_key_bindings = true
 config.window_background_opacity = 1;
+config.font_size = 11
 config.font =
     wezterm.font('JetBrains Mono')
 
@@ -19,4 +20,5 @@ config.font =
 
 -- config.default_prog = { "/bin/bash" }
 
+config.window_decorations = "NONE"
 return config

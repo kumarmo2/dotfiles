f228005e1dae2f785939398b0e6993db4c915e3b
kumarmo2 <kr.mohit6794@gmail.com>
2025-05-11 20:12:56 +0530

    fish and hyprland changes

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,13 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+
+
+# Set up fzf key bindings
+fzf --fish | source
+alias fzcd='pushd (fd --max-depth 4 -t d . "$HOME" | fzf --preview "ls {}")'
+alias fzn='nvim (fzf)'
+alias fzv='vim (fzf)'
+

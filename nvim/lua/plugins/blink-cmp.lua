f92dfae6be5aa151823afdfdc0c925867f54b486
kumarmo2 <kr.mohit6794@gmail.com>
2025-05-18 18:38:41 +0530

    updated blink-cmp and removed nvim-cmp

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -9,6 +9,19 @@ return { {
     keymap = {
       preset = 'default',
       ['<Tab>'] = {},
+      ['<C-j>'] = { 'select_next', 'fallback' },
+      ['<C-k>'] = { 'select_prev', 'fallback' },
+      ['<CR>'] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        'snippet_forward',
+        'fallback'
+      },
     },
 
     appearance = {

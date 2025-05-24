b25386c623471ff6e640d03a61254fda8282155a
kumarmo2 <kr.mohit6794@gmail.com>
2025-05-24 11:25:56 +0530

    some more neovim config changes.

--- nvim/after/ftplugin/zig.lua
+++ nvim/after/ftplugin/zig.lua
@@ -1,2 +1,22 @@
 vim.opt_local.makeprg = 'zig build'
 vim.opt_local.errorformat = "%f:%l:%c: %t%*[^:]: %m"
+
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>bp', ':Make<CR>', { silent = true, noremap = true });
+
+vim.api.nvim_create_user_command("Make",
+  function()
+    local logfile = "/tmp/zig_build.log"
+    print("Building Zig..")
+    vim.fn.jobstart("zig build > " .. logfile .. " 2>&1", {
+      on_exit = function(_, code)
+        if code == 0 then
+          print("✅ Zig build succeeded")
+        else
+          vim.cmd("vertical cfile " .. logfile)
+          vim.cmd("vertical topleft copen 80")
+          print("❌ Zig build failed. Check quickfix.")
+        end
+      end,
+    })
+  end, {}
+)
--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -6,10 +6,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank({ timeout = 500 })
   end,
 })
-
-vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("vertical topleft copen 80")
-  end,
-})

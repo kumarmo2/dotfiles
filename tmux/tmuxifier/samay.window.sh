6fec479b60e514ee9b3d0bbfb2a60eb6ecd5c622
kumarmo2 <kr.mohit6794@gmail.com>
2025-07-13 01:26:02 +0530

    updated some tmuxifier config

--- tmux/tmuxifier/samay.window.sh
+++ tmux/tmuxifier/samay.window.sh
@@ -1,10 +1,9 @@
-# Set window root path. Default is `$session_root`.
-# Must be called before `new_window`.
-window_root "~/dev/kumarmo2/chanakya/"
+
+window_root "~/dev/kumarmo2/samay"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
-new_window "chanakya"
+new_window "samay"
 
 split_h 70
 split_v 20
@@ -14,18 +13,18 @@ select_pane 0
 
 split_v 50
 
+select_pane 0
+run_cmd "cd backend/Scheduler.Api"
+run_cmd "dtr"
+
 select_pane 4
 
 run_cmd "envoy -c infra/envoy.yml"
 
 select_pane 3
 
-run_cmd "cd frontend/chanakya-ui"
+run_cmd "cd frontend/scheduler-ui"
 run_cmd "npm run dev"
-
-select_pane 0
-run_cmd "cd backend/go-api"
-run_cmd "go run *.go"
 
 select_pane 2
 run_cmd "nvim"

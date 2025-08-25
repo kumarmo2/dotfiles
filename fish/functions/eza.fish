eb2c9bdc3bbf40d276d780e33c852f39162cf9be
kumarmo2 <kr.mohit6794@gmail.com>
2025-08-25 20:00:56 +0530

    updated eza aliases

--- fish/functions/eza.fish
+++ fish/functions/eza.fish
@@ -1,8 +1,23 @@
 function ls
-    eza
+    eza --icons --group-directories-first $argv
 end
 
 function ll
-    eza -alh
+    eza -lh --icons --group-directories-first $argv
 end
 
+function la
+    eza -a --icons --group-directories-first $argv
+end
+
+function lla
+    eza -lah --icons --group-directories-first $argv
+end
+
+function lt
+    eza -T --icons $argv
+end
+
+function lf
+    eza -F --icons --group-directories-first $argv
+end

6564be4395306c8183c28ca381a4b11871c7a18c
kumarmo2 <kr.mohit6794@gmail.com>
2026-01-02 19:21:36 +0530

    some more changes for neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,7 +8,20 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     config = function()
-      vim.keymap.set("n", "<leader>n", "<Cmd>Neotree float toggle<CR>")
+      vim.keymap.set("n", "<leader>n", "<Cmd>Neotree float toggle reveal<CR>")
+      require('neo-tree').setup({
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            always_show_by_pattern = {
+              ".env*"
+            }
+          },
+          follow_current_file = {
+            enabled = true
+          }
+        }
+      })
     end,
     lazy = false, -- neo-tree will lazily load itself
   }

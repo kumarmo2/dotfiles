9f77959c64f7cdd940c85690e3965a0eba516632
kumarmo2 <kr.mohit6794@gmail.com>
2026-04-17 12:31:57 +0530

    updated tmux startup script

--- bin/tmux-startup.sh
+++ bin/tmux-startup.sh
@@ -2,19 +2,23 @@
 
 
 SESSION_NAME="multi_host"
+NOTES_SESSION_NAME="obsdian_notes"
 
 # NOTE: Create this file first and enter the password in it.
 pass=$(cat ~/dotfiles/scripts/bash/.ssh-pass)
 
-tmux new-session -d -s $SESSION_NAME -n host
+# Create obsdian_notes session
+tmux new-session -d -s $NOTES_SESSION_NAME -n notes -c $HOME/dev/test-vault/
+
+tmux new-window -t "$NOTES_SESSION_NAME:" -n agent -c $HOME/dev/test-vault/
+
+# Main session
+tmux new-session -d -s $SESSION_NAME -n host # -d means detached
+
 
 # Create additional windows
 tmux new-window -t "$SESSION_NAME:" -n pi
 tmux send-keys -t "$SESSION_NAME:pi" "sshpass -p '${pass}' ssh pi" C-m # C-m imitates enter press.
 
-# tmux new-window -t "$SESSION_NAME:" -n hp
-# tmux send-keys -t "$SESSION_NAME:hp" "sshpass -p '${pass}' ssh hp" C-m
-#
-#
 tmux swap-window -s "$SESSION_NAME:host" -t "$SESSION_NAME:pi"
 
